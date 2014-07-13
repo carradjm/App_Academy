257cf8013b3eb6bc25d92d9c29ad7fa230a30ab2
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-07-13 14:29:31 -0400

    started WordChainer

--- projects/word_chainer.rb
+++ projects/word_chainer.rb
@@ -1,2 +1,18 @@
-def word_chainer
+require 'set'
+
+class WordChainer
+
+  def initialize(dictionary_file_name)
+    @dictionary = File.readlines(dictionary_file_name).map {|x| x.chomp}
+    @dictionary = Set.new
+
+  end
+
+
+
+
+
+
+
+
 end
